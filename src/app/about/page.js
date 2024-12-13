97d119e7cbc6fb70372416498d4c2265588dadd1
npineda0 <97705502+npineda0@users.noreply.github.com>
2024-12-13 13:06:27 -0500

    quote issue fixed

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,10 +5,10 @@ export default function About() {
   return (
     <main className="aboutContainer">
         <p className="aboutSiteDesc">
-        This app incorporates Spotify's API and OAuth 2.0 to seamlessly integrate with your Spotify account,
+        This app incorporates Spotify&apos;s API and OAuth 2.0 to seamlessly integrate with your Spotify account,
         transforming your mood into a personalized playlist to match your unique vibe. By analyzing
         your mood and preferences, Mood Tunes suggests songs that you are most likely to enjoy. 
-        Among the sites features there's also an interactive customization option to further set the
+        Among the sites features there&apos;s also an interactive customization option to further set the
         mood by changing the color of the site. This app was created with Next.js and styled with SaSS. 
         </p>
 
@@ -20,11 +20,11 @@ export default function About() {
           <div className="aboutTxtContainer">
             <p>Naila Lugardo-Pineda</p>
             <p className="aboutMeTxt"> 
-            I'm an undergraduate student at Indiana University's Luddy School of Informatics for Media Arts and 
+            I&apos;m an undergraduate student at Indiana University&apos;s Luddy School of Informatics for Media Arts and 
             Science, specializing in a web design and development degree. As a Web Developer, my programming skills 
             include Javascript, PHP, React, and Next, along with experience using design softwares to create interactive
-            prototypes. In my projects, I've developed dynamic and responsive web applications featuring CRUD functionality,
-            MVC architecture, MySQL database management, and user authentication. I've incorporated libraries such as SaSS 
+            prototypes. In my projects, I&apos;ve developed dynamic and responsive web applications featuring CRUD functionality,
+            MVC architecture, MySQL database management, and user authentication. I&apos;ve incorporated libraries such as SaSS 
             and Bootstrap to effectively style these applications. 
             </p>
           </div>
